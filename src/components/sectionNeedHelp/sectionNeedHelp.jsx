ed6fcd0c656ed7c5c3afab9cd5ad7a8f066bf052
Chyharov <chyharov@gmail.com>
2024-11-11 23:48:34 +0100

    ADDED: styles and markup on sectionNeedHelp for desktop version

--- src/components/sectionNeedHelp/sectionNeedHelp.jsx
+++ src/components/sectionNeedHelp/sectionNeedHelp.jsx
@@ -1,10 +1,12 @@
+import { FiPhoneCall } from "react-icons/fi";
 import s from './sectionNeedHelp.module.scss';
 
 const sectionNeedHelp = () => {
   return (
     <section className={s.sectionNeedHelp}>
       <div className="container">
-        <h2>sectionNeedHelp</h2>
+        <h2 className={s.titleForNeedHelp}>Потрібна допомога? Зателефонуйте нам...</h2>
+        <a className={s.needHelpLink} href="tel:+380939040707"><FiPhoneCall className={s.iconPhone} /><p className={s.phoneForNeedHelp}>+38 (093) 904-07-07</p></a>
       </div>
     </section>
   );
