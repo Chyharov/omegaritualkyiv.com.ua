f4be9cf61221430507de82e99cb2a3e9fc373a6f
Chyharov <chyharov@gmail.com>
2025-01-09 23:38:52 +0100

    ADDED: markup and styles on sectionOrganizationFuneral for desktop version

--- src/components/sectionOrganizationFuneral/sectionOrganizationFuneral.jsx
+++ src/components/sectionOrganizationFuneral/sectionOrganizationFuneral.jsx
@@ -1,13 +1,27 @@
+import OrganizationFuneralImage from 'images/organizationFuneral/5.jpg'
 import s from './sectionOrganizationFuneral.module.scss';
 
 const sectionOrganizationFuneral = () => {
   return (
     <section className={s.sectionOrganizationFuneral}>
       <div className="container">
-        <h1>Організація ритуальних послуг</h1>
-        <p>Починаючи з оформлення документів і закінчуючи поминальним обідом, такий стрес може негативно позначитися на здоров'ї людини.</p>
-        <p>Ми працюємо з усіма клієнтами і знаходимо їм найкращий варіант. Ми співпрацюємо з десятками ресторанів, маємо власний транспорт, тому ціна наших ритуальних послуг завжди залишається низькою і доступною для всіх.</p>
-      <button>докладніше</button>
+        <div className={s.organizationFuneralContainer}>
+          <img loading='lazy' className={s.organizationFuneralImage} src={OrganizationFuneralImage} alt="OrganizationFuneralImage" />
+          <div className={s.organizationFuneralDescription}>
+            <h1>Організація ритуальних послуг</h1>
+          <p>
+            Починаючи з оформлення документів і закінчуючи поминальним обідом,
+            такий стрес може негативно позначитися на здоров'ї людини.
+          </p>
+          <p>
+            Ми працюємо з усіма клієнтами і знаходимо їм найкращий варіант. Ми
+            співпрацюємо з десятками ресторанів, маємо власний транспорт, тому
+            ціна наших ритуальних послуг завжди залишається низькою і доступною
+            для всіх.
+          </p>
+          <button>докладніше</button>
+          </div>
+        </div>
       </div>
     </section>
   );
