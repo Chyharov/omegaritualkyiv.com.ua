79f706b2ccb40f94e4781a1235224e837fba9616
Chyharov <chyharov@gmail.com>
2025-01-14 22:38:44 +0100

    ADDED: styles on sectionOrganizationFuneral for desktop version

--- src/components/sectionOrganizationFuneral/sectionOrganizationFuneral.jsx
+++ src/components/sectionOrganizationFuneral/sectionOrganizationFuneral.jsx
@@ -8,18 +8,17 @@ const sectionOrganizationFuneral = () => {
         <div className={s.organizationFuneralContainer}>
           <img loading='lazy' className={s.organizationFuneralImage} src={OrganizationFuneralImage} alt="OrganizationFuneralImage" />
           <div className={s.organizationFuneralDescription}>
-            <h1>Організація ритуальних послуг</h1>
-          <p>
-            Починаючи з оформлення документів і закінчуючи поминальним обідом,
-            такий стрес може негативно позначитися на здоров'ї людини.
+            <h1 className={s.titleOrganizationFuneral}>Організація ритуальних послуг</h1>
+          <p className={s.descriptionOrganizationFuneral}>
+            Починаючи з оформлення документів і закінчуючи поминальним обідом. Такий стрес може негативно вплинути на здоров'я людини. Омега-Ритуал понад 15 років працює у сфері ритуальних послуг і ми знаємо як організувати похорон за доступною для вас ціною.
           </p>
-          <p>
+          <p className={s.descriptionOrganizationFuneral}>
             Ми працюємо з усіма клієнтами і знаходимо їм найкращий варіант. Ми
             співпрацюємо з десятками ресторанів, маємо власний транспорт, тому
             ціна наших ритуальних послуг завжди залишається низькою і доступною
             для всіх.
           </p>
-          <button>докладніше</button>
+          <button className={s.buttonOrganizationFuneral}>ДОКЛАДНІШЕ</button>
           </div>
         </div>
       </div>
