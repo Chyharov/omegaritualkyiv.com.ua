36bd2eca80830a3403b9964c8b7a7a176c509b27
chyharov <chyharov@gmail.com>
2025-05-16 22:10:24 +0200

    ADDED: logo on header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,12 @@
+import logo from 'images/header/logo.svg'
 import s from './header.module.scss';
 
 const header = () => {
   return (
     <header className={s.header}>
       <div className={'container ' + s.header__padding}>
+
+        <img src={logo} alt="logo__header" />
 
         <h1 className={s.header__title}>header</h1>
         
