ca329003ac43032d219576940a7ed21dd8fb1e7a
chyharov <chyharov@gmail.com>
2025-07-25 18:12:43 +0200

    added: tag link for logo on header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import logo from 'images/header/logo.svg'
 import NavBar from 'components/NavBar/NavBar'
 import s from './header.module.scss';
@@ -6,8 +7,9 @@ const header = ({navItems}) => {
   return (
     <header className={s.header}>
       <div className={`container ${s.header__container}`}>
-
-        <img className={s.header__logo} src={logo} alt="header__logo" />
+        <Link to="/">
+          <img className={s.header__logo} src={logo} alt="header__logo" />
+        </Link>
 
         <NavBar navItems={navItems} />
         
