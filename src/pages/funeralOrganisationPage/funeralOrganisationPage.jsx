c19c2b3a7404206bdc322afc9016d93a95b0cfb7
chyharov <chyharov@gmail.com>
2025-08-30 20:30:26 +0200

    update: markup SectionFuneralOrganisation and styles on _textVariables

--- src/pages/funeralOrganisationPage/funeralOrganisationPage.jsx
+++ src/pages/funeralOrganisationPage/funeralOrganisationPage.jsx
@@ -1,9 +1,9 @@
 import { useState, useCallback } from 'react';
 import Header from 'components/header/header';
-import SectionRitualAgent from 'components/SectionRitualAgent/SectionRitualAgent';
+import SectionFuneralOrganisation from 'components/SectionFuneralOrganisation/SectionFuneralOrganisation';
 import Footer from 'components/footer/footer';
 import translations from 'components/LanguageSelect/translations';
-import imgBaner from 'images/sectionRitualAgent/ritualagent.jpg';
+import imgBaner from 'images/funeralOrganisationPage/funeralOrganisationPage.jpg';
 
 const FuneralOrganisationPage = () => {
   const [language, setLanguage] = useState('ua');
@@ -29,11 +29,13 @@ const FuneralOrganisationPage = () => {
   );
 
   const title = 'Організатор похорону';
+  const imgBannerDescription = 'Смерть близької людини – великий стрес для рідних. Він буквально паралізує свідомість і позбавляє всяких моральних та фізичних сил займатися організацією похорону. Ритуальне бюро «Омега» бере на себе всі організаційні питання щодо планування та контролю за проведенням усіх етапів церемонії прощання з покійним, включаючи оформлення документів та ведення переговорів щодо дозволу поховання на закритих цвинтарях Києва.';
+
   return (
     <>
       <Header navItems={navItems} handleLanguageChange={handleLanguageChange} language={language} />
       <main>
-        <SectionRitualAgent title={title} imgBaner={imgBaner} />
+        <SectionFuneralOrganisation title={title} imgBaner={imgBaner} imgBannerDescription={imgBannerDescription} />
       </main>
       <Footer />
     </>
