036c31db43b55f9ac2e83080cddd2725d4d68c23
chyharov <chyharov@gmail.com>
2026-01-03 22:36:31 +0100

    added: SectionServiceList to SectionOrganizationFuneralServices

--- src/components/SectionOrganizationFuneralServices/SectionOrganizationFuneralServices.jsx
+++ src/components/SectionOrganizationFuneralServices/SectionOrganizationFuneralServices.jsx
@@ -1,14 +1,16 @@
 import TitleForPage from '../TitleForPage/TitleForPage';
+import SectionServiceList from '../sectionServiceList/sectionServiceList.jsx';
 import s from './SectionOrganizationFuneralServices.module.scss';
 
-const SectionOrganizationFuneralServices = ({ title, imgBaner, imgBannerDescription }) => {
+const SectionOrganizationFuneralServices = ({ title }) => {
   return (
     <section className={s.sectionOrganizationFuneralServices}>
-      <div className={`container ${s.sectionOrganizationFuneralServices__container}`}>
+      <div
+        className={`container ${s.sectionOrganizationFuneralServices__container}`}
+      >
         <TitleForPage title={title} />
 
-        <h2 className="title">OrganizationFuneralServicesPage</h2>
-
+        <SectionServiceList />
       </div>
     </section>
   );
