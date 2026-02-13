e66e3576e22a82287ef19b154c461ac013492153
chyharov <chyharov@gmail.com>
2026-02-13 23:12:21 +0100

    added: SectionServiceList to SectionFuneralPortrait

--- src/components/SectionFuneralPortrait/SectionFuneralPortrait.jsx
+++ src/components/SectionFuneralPortrait/SectionFuneralPortrait.jsx
@@ -1,6 +1,130 @@
 import TitleForPage from '../TitleForPage/TitleForPage';
 import BanerForSection from 'components/BanerForSection/BanerForSection';
+import SectionServiceList from 'components/sectionServiceList/sectionServiceList';
 import s from './SectionFuneralPortrait.module.scss';
+
+  const serviceList = [
+    {
+      id: 1,
+      src: require('../../images/servicesList/01.png'),
+      alt: 'Зображення 1',
+      title: 'Ритуальний агент',
+      link: '/ritualagent',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 2,
+      src: require('../../images/servicesList/02.png'),
+      alt: 'Зображення 2',
+      title: 'Організація похорон',
+      link: '/funeralorganisation',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 3,
+      src: require('../../images/servicesList/03.png'),
+      alt: 'Зображення 3',
+      title: 'Кремація',
+      link: '/cremationservices',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 4,
+      src: require('../../images/servicesList/04.png'),
+      alt: 'Зображення 4',
+      title: 'Ритуальний транспорт',
+      link: '/ritualtransport',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 5,
+      src: require('../../images/servicesList/05.png'),
+      alt: 'Зображення 5',
+      title: 'Відспівування померлого',
+      link: '/funeralofthedeceased',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 6,
+      src: require('../../images/servicesList/06.png'),
+      alt: 'Зображення 6',
+      title: 'Вантаж 200',
+      link: '/transportationofthedeceased',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 7,
+      src: require('../../images/servicesList/07.png'),
+      alt: 'Зображення 7',
+      title: 'Послуги моргу',
+      link: '/mortuaryservices',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 8,
+      src: require('../../images/servicesList/08.png'),
+      alt: 'Зображення 8',
+      title: 'Поминальні обіди',
+      link: '/memorialdinners',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 9,
+      src: require('../../images/servicesList/09.png'),
+      alt: 'Зображення 9',
+      title: 'Оформлення документів',
+      link: '/processingdocuments',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 10,
+      src: require('../../images/servicesList/10.png'),
+      alt: 'Зображення 10',
+      title: 'Ритуальна зала',
+      link: '/ritualhall',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 11,
+      src: require('../../images/servicesList/11.png'),
+      alt: 'Зображення 11',
+      title: 'Музичний супровід',
+      link: '/funeralmusic',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 12,
+      src: require('../../images/servicesList/12.png'),
+      alt: 'Зображення 12',
+      title: 'Супровід медперсоналом',
+      link: '/medicalsupportonfuneral',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 13,
+      src: require('../../images/servicesList/13.png'),
+      alt: 'Зображення 13',
+      title: 'Носії труни',
+      link: '/pallbearers',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 14,
+      src: require('../../images/servicesList/14.png'),
+      alt: 'Зображення 14',
+      title: 'Ведучий на похорон',
+      link: '/funeralhost',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+    {
+      id: 15,
+      src: require('../../images/servicesList/15.png'),
+      alt: 'Зображення 15',
+      title: 'Дезобробка',
+      link: '/disinfection',
+      linkTitle: 'ДЕТАЛЬНІШЕ',
+    },
+  ];
 
 const SectionFuneralPortrait = ({
   title,
@@ -41,6 +165,9 @@ const SectionFuneralPortrait = ({
               </ul>
 
               <p className='description'>Будь-який портрет обрамляють у кадрі. Це може бути як елітний різьблений дерев'яний багет із позолоченням, так і зовсім недорогі бюджетні обрамлення. У будь-якому разі, яким би не був портрет на похороні, він виконуватиме свою сакральну функцію, символізуючи вічне життя покійного в пам'яті живих, і виражатиме всю повноту поваги до покійного.</p>
+      
+        <SectionServiceList serviceList={serviceList} />
+      
       </div>
     </section>
   );
